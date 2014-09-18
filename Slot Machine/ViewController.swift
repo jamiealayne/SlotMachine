0c399d7439420e1583473bba895fe631c2e88ba7
Jamie Layne <jamielayne@Jamies-MacBook-Pro.local>
2014-09-18 16:46:56 -0400

    Set up second 2 container views

--- Slot Machine/ViewController.swift
+++ Slot Machine/ViewController.swift
@@ -33,17 +33,35 @@ class ViewController: UIViewController {
 
 
 	func setUpContainerViews() {
+		// First Container Config
 		self.firstContainer = UIView(frame:
 			CGRectMake(self.view.bounds.origin.x + VIEW_MARGIN, self.view.bounds.origin.y, self.view.bounds.width - (2 * VIEW_MARGIN), self.view.bounds.height * SIXTH))
 		
 		self.firstContainer.backgroundColor = UIColor.redColor()
 		self.view.addSubview(self.firstContainer)
 		
+		// Second Container Config
 		self.secondContainer = UIView(frame:
 			CGRectMake(self.view.bounds.origin.x + VIEW_MARGIN, self.firstContainer.frame.height, self.view.bounds.width - (2 * VIEW_MARGIN), self.view.bounds.height * (3 * SIXTH) ) )
 		
-		self.secondContainer.backgroundColor = UIColor.redColor()
+		self.secondContainer.backgroundColor = UIColor.blackColor()
 		self.view.addSubview(self.secondContainer)
+		
+		// Third Container Config
+		self.thirdContainer = UIView(frame:
+			CGRectMake(self.view.bounds.origin.x + VIEW_MARGIN, self.firstContainer.frame.height + secondContainer.frame.height, self.view.bounds.width - (2 * VIEW_MARGIN), self.view.bounds.height * SIXTH ) )
+		
+		self.thirdContainer.backgroundColor = UIColor.lightGrayColor()
+		self.view.addSubview(self.thirdContainer)
+		
+		// Fourth Container Config
+		self.fourthContainer = UIView(frame:
+			CGRectMake(self.view.bounds.origin.x + VIEW_MARGIN, self.firstContainer.frame.height + secondContainer.frame.height + thirdContainer.frame.height, self.view.bounds.width - (2 * VIEW_MARGIN), self.view.bounds.height * SIXTH ) )
+		
+		self.fourthContainer.backgroundColor = UIColor.lightGrayColor()
+		self.view.addSubview(self.fourthContainer)
+
+		
 	}
 }
 
