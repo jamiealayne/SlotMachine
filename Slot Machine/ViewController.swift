c90b6da14b0a10c02ea019e021d06c46fceb1c25
Jamie Layne <jamielayne@Jamies-MacBook-Pro.local>
2014-09-18 16:40:06 -0400

    Set up first 2 container views

--- Slot Machine/ViewController.swift
+++ Slot Machine/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	// Container Views
+	var firstContainer: UIView!
+	var secondContainer: UIView!
+	var thirdContainer: UIView!
+	var fourthContainer: UIView!
+	
+	// Constants 
+	let VIEW_MARGIN: CGFloat = 10.0
+	let SIXTH:CGFloat = 1.0/6.0
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +32,18 @@ class ViewController: UIViewController {
 	}
 
 
+	func setUpContainerViews() {
+		self.firstContainer = UIView(frame:
+			CGRectMake(self.view.bounds.origin.x + VIEW_MARGIN, self.view.bounds.origin.y, self.view.bounds.width - (2 * VIEW_MARGIN), self.view.bounds.height * SIXTH))
+		
+		self.firstContainer.backgroundColor = UIColor.redColor()
+		self.view.addSubview(self.firstContainer)
+		
+		self.secondContainer = UIView(frame:
+			CGRectMake(self.view.bounds.origin.x + VIEW_MARGIN, self.firstContainer.frame.height, self.view.bounds.width - (2 * VIEW_MARGIN), self.view.bounds.height * (3 * SIXTH) ) )
+		
+		self.secondContainer.backgroundColor = UIColor.redColor()
+		self.view.addSubview(self.secondContainer)
+	}
 }
 
